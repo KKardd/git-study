e225c3f713f2ae7ac007b08b1e7de53d74752e6f
KKardd <wlstjd4013@gmail.com>
2023-03-27 15:34:28 +0900

    commit 1

--- test_code.js
+++ test_code.js
@@ -1,6 +1,10 @@
 function print(n) {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < n; i++) {
         console.log(i);
     }
 }
 // n을 입력하면 0부터 n까지 출력해주는 함수 - commit 1
+// ...
+// ...
+// ...
+// 기능추가
