3c6928e219ceb039ea5266d1e93d5c16ed6bd972
KKardd <wlstjd4013@gmail.com>
2023-03-28 21:06:26 +0900

    commit 2

--- testcode2.js
+++ testcode2.js
@@ -6,3 +6,12 @@ function print(n) {
     }
 }
 // n을 입력하면 0부터 n까지 출력해주는 함수 - commit 1
+
+function printOdd(n) {
+    for (let i = 0; i < n; i++) {
+        if (i % 2 === 1) {
+            console.log(i);
+        }
+    }
+}
+// n을 입력하면 0부터 n까지 숫자중 홀수를 출력해주는 함수 - commit 2
