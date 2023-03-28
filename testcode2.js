202f144cae61808a02833125d2ff10fb94ecd48d
KKardd <wlstjd4013@gmail.com>
2023-03-28 21:11:51 +0900

    commit 3

--- testcode2.js
+++ testcode2.js
@@ -15,3 +15,17 @@ function printOdd(n) {
     }
 }
 // n을 입력하면 0부터 n까지 숫자중 홀수를 출력해주는 함수 - commit 2
+
+// 홀수들의 합을 구하는 기능을 만들기 위해 Odd branch 개설
+
+function EvenSum(n) {
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        if (i % 2 === 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+// 그 도중 다른 기능(짝수들의 합을 구하는 기능)은 메인 branch에서 개발 commit 3
